06d8f89091fe1351c6f6368b5888701ef73306b0
Nerby <t2nerb@gmail.com>
2017-07-10 00:17:41 -0600

    Cringe-worthy, the good ole' days

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+About
+---------------
+Scroll your car left and right to avoid the blocks! To play, just install the dependencies with `pip3 install -r requirements.txt` then `python3 a_pygame.py`.  
+
+#### Fun Fact
+A memento to the first _game_ I ever made. I know... it's rough. 
+
 Dependencies
 ----------------
 * Python 3.4.2
-* Pygame 1.9.2a0
+* Pygame 1.9.2b6
+
 
--- apygame.py
+++ apygame.py
@@ -1,3 +1,4 @@
+#!/usr/local/bin/python3
 import pygame,time,random
 
 pygame.init()
@@ -15,105 +16,100 @@ green = (0,255,0)
 blue = (0,0,255)
 
 gameDisplay = pygame.display.set_mode((disp_width,disp_height))
-pygame.display.set_caption('Don\'t ask wtf this is')
+pygame.display.set_caption('The good ole\' days')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('car.png')
 
 def show_score(score):
-	size = pygame.font.SysFont(None, 25)
-	txt = ('Dodged: ' + str(score))
-	text = font.render(txt, True, black)
-	gameDisplay.blit(text, (0,0))
-	
+    size = pygame.font.SysFont(None, 25)
+        txt = ('Dodged: ' + str(score))
+        text = font.render(txt, True, black)
+        gameDisplay.blit(text, (0,0))
+
 def obstacles(obs_x, obs_y, obs_width, obs_height, color):
-	pygame.draw.rect(gameDisplay, color, [obs_x, obs_y, obs_width, obs_height])
+    pygame.draw.rect(gameDisplay, color, [obs_x, obs_y, obs_width, obs_height])
 
 def car(x,y):
-	gameDisplay.blit(carImg, (x,y))
+    gameDisplay.blit(carImg, (x,y))
 
 def text_objects(text, info):
-	textSurface = info.render(text,True,red)
-	return textSurface, textSurface.get_rect()
+    textSurface = info.render(text,True,red)
+        return textSurface, textSurface.get_rect()
 
 def message_display(text):
-	size = pygame.font.Font('freesansbold.ttf', 115)
-	TextSurf, TextRect = text_objects(text, size)
-	TextRect.center = ((disp_width/2),(disp_height/2))
-	gameDisplay.blit(TextSurf, TextRect)
-	pygame.display.update()
+    size = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects(text, size)
+        TextRect.center = ((disp_width/2),(disp_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+        pygame.display.update()
 
 def crash():
-	message_display('CRASHED!!!')
-
-
+    message_display('CRASHED!!!')
 
 def gameloop():
-	counter = 0
-	dodged = 0
-	x = (disp_width * 0.5)
-	y = (disp_height * 0.8)
-	car_xleft = 0
-	car_xright = 0
+    dodged = 0
+    x = (disp_width * 0.5)
+    y = (disp_height * 0.8)
+    car_xleft = 0
+    car_xright = 0
 
-	obs_xcoord = random.randrange(0,disp_width)
-	obs_ycoord = -500
-	obs_speed = 20
-	obs_width = 40
-	obs_height = 80
-	
+    obs_xcoord = random.randrange(0,disp_width)
+    obs_ycoord = -500
+    obs_speed = 20
+    obs_width = 40
+    obs_height = 80
 
-	alive = True 
-	while alive:
-		counter += 1
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				alive = False 
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_LEFT:
-					car_xleft -= 30 
-				elif event.key == pygame.K_RIGHT:
-					car_xright += 30 
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_LEFT:
-					car_xleft = 0
-				if event.key == pygame.K_RIGHT:
-					car_xright = 0
 
-		car_xdisp = car_xleft + car_xright
-		if (x + car_xdisp) < (disp_width-37) and (x + car_xdisp) > 0:
-			x += car_xdisp
-			
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_UP:
-				crash()
-				time.sleep(3)
-				alive = False
+    alive = True
+    while alive:
+        counter += 1
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    alive = False
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_LEFT:
+                            car_xleft -= 30
+                        elif event.key == pygame.K_RIGHT:
+                            car_xright += 30
+                    if event.type == pygame.KEYUP:
+                        if event.key == pygame.K_LEFT:
+                            car_xleft = 0
+                            if event.key == pygame.K_RIGHT:
+                                car_xright = 0
 
-		gameDisplay.fill(white)
+            car_xdisp = car_xleft + car_xright
+            if (x + car_xdisp) < (disp_width-37) and (x + car_xdisp) > 0:
+                x += car_xdisp
 
-		#def obstacles(obs1, obs2, obs_width, obs_height, color):
-		obstacles(obs_xcoord, obs_ycoord, obs_width, obs_height, black)
-		obs_ycoord += obs_speed
-		car(x,y)
-		#show_score(dodged)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    crash()
+                    time.sleep(3)
+                    alive = False
 
-		if obs_ycoord > disp_height:
-			obs_ycoord = 0 - obs_height
-			obs_xcoord = random.randrange(0,disp_width)
-			obs_speed += 1
-			dodged += 1
-		if y < obs_ycoord+obs_height:
-			if x > obs_xcoord and x < obs_xcoord + obs_width or x+car_width > obs_xcoord and x + car_width < obs_xcoord:
-				crash()
-				time.sleep(3)
-				alive = False
-				
-		pygame.display.update()
-		print(event)
-		clock.tick(120)
+            gameDisplay.fill(white)
+
+            #def obstacles(obs1, obs2, obs_width, obs_height, color):
+            obstacles(obs_xcoord, obs_ycoord, obs_width, obs_height, black)
+            obs_ycoord += obs_speed
+            car(x,y)
+            #show_score(dodged)
+
+            if obs_ycoord > disp_height:
+                obs_ycoord = 0 - obs_height
+                    obs_xcoord = random.randrange(0,disp_width)
+                    obs_speed += 1
+                    dodged += 1
+            if y < obs_ycoord+obs_height:
+                if x > obs_xcoord and x < obs_xcoord + obs_width or x+car_width > obs_xcoord and x + car_width < obs_xcoord:
+                    crash()
+                        time.sleep(3)
+                        alive = False
+
+            pygame.display.update()
+            #print(event)
+            clock.tick(120)
 gameloop()
 pygame.quit()
 quit()
-	
-
